f7a407044fd9aafb62430200230b9bad3927bd63
Junaid Syed <sjunaid626@gmail.com>
2023-09-27 11:42:53 +0530

    Update cors.rb

--- config/environments/cors.rb
+++ config/environments/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3001'  # Update with your frontend's URL
-    resource '/api/*',
+    resource '/api/greetings',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       expose: ['Access-Token', 'Uid', 'Client'] # Customize this based on your needs
